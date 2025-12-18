d647259616d792bf640313dc27abe391a8a3c6b0
Ricardo Banon <ricardo.banon@1global.com>
2025-12-18 21:14:08 +0000

    Updated karabiner, nvim and kitty

--- nvim/.config/nvim/lua/plugins/dadbod.lua
+++ nvim/.config/nvim/lua/plugins/dadbod.lua
@@ -12,5 +12,10 @@ return {
 	},
 	init = function()
 		vim.g.db_ui_use_nerd_fonts = 1
+		-- Load database connections from external file
+		local db_file = vim.fn.expand('~/.config/nvim/db_connections.lua')
+		if vim.fn.filereadable(db_file) == 1 then
+			dofile(db_file)
+		end
 	end,
 }
