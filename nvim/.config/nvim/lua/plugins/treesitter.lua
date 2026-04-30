780f0fd65f6dcc0c4fa968fa3277e73747404d7e
Ricardo Banon <ricardo.banon@1global.com>
2026-04-30 16:36:27 +0100

    Fix issues with color scheme; Changed color scheme from nightfly to Kanagawa-Wave with custom background color

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,19 +1,22 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = function()
-		local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
-		ts_update()
-	end,
-	config = {
-		ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
-
-		sync_install = false,
-		auto_install = true,
-
-		highlight = {
-			enable = true,
-			additional_vim_regex_highlighting = false,
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		branch = "main",
+		lazy = false,
+		opts = {
+			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+			auto_install = true,
 		},
+		config = function(_, opts)
+			require("nvim-treesitter").setup(opts)
+			-- The new nvim-treesitter main branch no longer auto-attaches highlights.
+			-- Use neovim's built-in treesitter API to attach on every FileType event.
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function(ev)
+					pcall(vim.treesitter.start, ev.buf)
+				end,
+			})
+		end,
 	},
-	branch = "main",
 }
